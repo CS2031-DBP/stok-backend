b8cc1a30d53566113447400b77bf78a8187c6a92
Joel_Miguel <joel.miguel@utec.edu.pe>
2024-07-13 23:03:47 -0500

    vamo

--- src/main/java/com/example/stokapp/auth/dto/AuthRegisterRequest.java
+++ src/main/java/com/example/stokapp/auth/dto/AuthRegisterRequest.java
@@ -1,13 +1,20 @@
 package com.example.stokapp.auth.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.NonNull;
 
 @Data
 public class AuthRegisterRequest {
+    @NotNull
     String firstName;
+    @NotNull
     String lastName;
+    @NotNull
     String email;
+    @NotNull
     String password;
     Boolean isOwner = false;
+    @NotNull
     String phoneNumber;
 }
